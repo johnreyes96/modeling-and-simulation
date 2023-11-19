7c45ec72c9c9e00ec38d2f98cb83b1c1b7c1b466
John Reyes <jhonfer96@hotmail.com>
2023-11-19 01:07:46 -0500

    :art: Format: add format to python code

--- scripts/classes/Probability.py
+++ scripts/classes/Probability.py
@@ -8,7 +8,8 @@ Original file is located at
 
 # Visualización con Matplotlib
 
-Matplotlib es la librería fundamental de visualización en Python. Es una librería de bajo nivel, con muchísima flexibilidad, de sintaxis compleja pero [documentada](https://matplotlib.org/users/index.html).
+Matplotlib es la librería fundamental de visualización en Python. Es una librería de bajo nivel, con muchísima
+flexibilidad, de sintaxis compleja pero [documentada](https://matplotlib.org/users/index.html).
 
 ### 1. Gráficos de Líneas y Puntos
 Vamos a graficar una función sinusoidal de tipo: $y = sen(2 \pi x) + 1$.
@@ -16,6 +17,7 @@ Vamos a graficar una función sinusoidal de tipo: $y = sen(2 \pi x) + 1$.
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 # Generamos dos vectores que nos daran las cordenadas por donde pasará la funciòn
 x = np.arange(0.0, 2.0, 0.05)
@@ -33,7 +35,8 @@ plt.plot(x, y, 'o')
 
 plt.plot(x, y, 'o-')
 
-"""Muy parecida es la función `plt.scatter()`, que sirve para hacer gráficos de dispersión. A diferencia de `plt.plot()`, esta función no permite gráficos de líneas."""
+"""Muy parecida es la función `plt.scatter()`, que sirve para hacer gráficos de dispersión. A diferencia de 
+`plt.plot()`, esta función no permite gráficos de líneas."""
 
 plt.scatter(x, y)
 
@@ -59,7 +62,8 @@ plt.show()  # Show muestra el gráfico.
 
 """**Leyendas**
 
-En general, los gráficos suelen contener más de una curva. Para poder distinguirlas correctamente, usamos leyendas, que son cuadros que se agregan dentro del gráfico que nos brindan información sobre los distintos elementos.
+En general, los gráficos suelen contener más de una curva. Para poder distinguirlas correctamente, usamos leyendas, 
+que son cuadros que se agregan dentro del gráfico que nos brindan información sobre los distintos elementos.
 
 Cómo graficar más de una curva con sus correspondientes etiquetas (`label`) y cómo crear la leyenda.
 """
@@ -85,9 +89,6 @@ plt.legend()  # loc='upper center', shadow=True, fontsize='x-large')plt.show()
 """### 3. Otra manera de gráficar
 *Usando* la función `plt.subplot()`.
 """
-
-import numpy as np
-import matplotlib.pyplot as plt
 
 x = np.linspace(0, 10, 50)
 y = np.sin(x)
@@ -167,19 +168,17 @@ ax = plt.axes()
 ax.plot(x, y)
 ax.plot(x2, y2)
 
-"""En la siguiente pagina pueden encontrar mas ejemplos de como utilizar Matplotlib [tutorial](https://matplotlib.org/3.1.3/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py).
+"""En la siguiente pagina pueden encontrar mas ejemplos de como utilizar Matplotlib [tutorial](
+https://matplotlib.org/3.1.3/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py).
 
 #Probabilidad y Estadística
 
-## 1. Distribuciones de probabilidad
-El concepto de distribución de probabilidad es un concepto central en probabilidad y estadística y, por lo tanto, hay mucho para decir. Nos limitamos acá a los puntos más importantes.
+## 1. Distribuciones de probabilidad El concepto de distribución de probabilidad es un concepto central en 
+probabilidad y estadística y, por lo tanto, hay mucho para decir. Nos limitamos acá a los puntos más importantes.
 
-### 1.1. Distrubución Uniforme Discreta
-Comencemos por un ejemplo: un dado de seis caras. La probabilidad de que al tirar el dado salga una cara es  1/6 . Si graficamos la probabilidad para cada resultado posible de tirar un dado, obtendríamos un gráfico como el siguiente:
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
+### 1.1. Distrubución Uniforme Discreta Comencemos por un ejemplo: un dado de seis caras. La probabilidad de que al 
+tirar el dado salga una cara es  1/6 . Si graficamos la probabilidad para cada resultado posible de tirar un dado, 
+obtendríamos un gráfico como el siguiente:"""
 
 valores = np.arange(1, 7)
 probas = np.zeros(6) + 1 / 6
@@ -188,25 +187,31 @@ plt.title('Distribución de probabilidad uniforme: lanzamiento de un dado')
 # plt.savefig('distribucion_dado.png', dpi = 400)
 plt.show()
 
-"""En este caso, decimos que la distribución de probabilidad es *uniforme discreta*, ya que le asigna la misma probabilidad a los seis valores que pueden salir al tirar el dado. Si el dado estuviera cargado, ya no sería uniforme.
+"""En este caso, decimos que la distribución de probabilidad es *uniforme discreta*, ya que le asigna la misma 
+probabilidad a los seis valores que pueden salir al tirar el dado. Si el dado estuviera cargado, ya no sería uniforme.
 
 **Algunos detalles**:
 1. El resultado de tirar un dado es un ejemplo de una *variable aleatoria*.
-2. En el caso del dado, la variable aleatoria puede tomar valores *discretos* y *acotados* (limitados): 1, 2, 3, 4, 5 y 6
+2. En caso del dado, la variable aleatoria puede tomar valores *discretos* y *acotados* (limitados): 1, 2, 3, 4, 5 y 6
 3. Existen variables aleatorias donde los posibles valores que puede tomar son continuos y no acotados.
 
 ### 1.2 Distribución Normal o Gaussiana
 
-La distribución normal o gaussiana debe ser la distribución más famosa dentro de las distribuciones. Es una distribución de variable continua y aparece en una infinidad de ámbitos de la ciencia. Muchas variables asociadas a fenómenos naturales siguen una distribución gaussiana; un ejemplo típico es la estatura de las personas. La forma que tiene esta distribución está dada por la siguiente fórmula:
+La distribución normal o gaussiana debe ser la distribución más famosa dentro de las distribuciones. Es una 
+distribución de variable continua y aparece en una infinidad de ámbitos de la ciencia. Muchas variables asociadas a 
+fenómenos naturales siguen una distribución gaussiana; un ejemplo típico es la estatura de las personas. La forma que 
+tiene esta distribución está dada por la siguiente fórmula:
 
 $$f(x|\mu, \sigma^2)=\frac{1}{\sqrt{2 \pi \sigma^2}}e^{\frac{-(x - \mu)^2}{2\sigma^2}}$$
 
-Es importante resaltar que tiene sólo dos parámetros: su valor medio $\mu$ y su desviacíon estándar $\sigma$. Estos valores son *teóricos*, es decir, son propios de la distribución de probabilidad.
+Es importante resaltar que tiene sólo dos parámetros: su valor medio $\mu$ y su desviacíon estándar $\sigma$. Estos 
+valores son *teóricos*, es decir, son propios de la distribución de probabilidad.
 
 **Distribución Normal en NumPy**
 
-NumPy nos provee de herramientas para generar valores aleatorios de distribuciones. A continuación generamos, usando `np.random.normal()`, muestras de dos distribuciones normales, con el mismo valor medio pero distinta desviación estándar.
-"""
+NumPy nos provee de herramientas para generar valores aleatorios de distribuciones. A continuación generamos, 
+usando `np.random.normal()`, muestras de dos distribuciones normales, con el mismo valor medio pero distinta 
+desviación estándar."""
 
 mu = 2.0
 sigma_1 = 5.0
@@ -215,7 +220,8 @@ muestras_1 = np.random.normal(loc=mu, scale=sigma_1, size=400)
 muestras_2 = np.random.normal(loc=mu, scale=sigma_2, size=400)
 print(muestras_1, muestras_2)
 
-"""Para visualizar los datos anteriores lo mejor es utilizar un histograma y lo que hace es tomar un número determinado de intervalos (bins = 20) y contar cuántas muestras caen en cada intervalo."""
+"""Para visualizar los datos anteriores lo mejor es utilizar un histograma y lo que hace es tomar un número 
+determinado de intervalos (bins = 20) y contar cuántas muestras caen en cada intervalo."""
 
 plt.hist(muestras_1, bins=20, alpha=0.5, label='Histrograma Muestra 1')
 plt.hist(muestras_2, bins=20, alpha=0.5, label='Histrograma Muestra 2')
@@ -227,7 +233,10 @@ plt.show()
 
 **Promedio y desviación estándar en una distribución Normal**
 
-En una distribución normal, el promedio de las muestras obtenidas *tiende* al valor medio $\mu$ de la distribución, y la desviación estándar *tiende* a la desviacíon estándar $\sigma$ de la distribución. Notar, entonces, que existen valores calculados (promedio, desviación estándar) y valores teóricos ($\mu$ y $\sigma$). Confundirlos entre sí es un error común.
+En una distribución normal, el promedio de las muestras obtenidas *tiende* al valor medio $\mu$ de la distribución, 
+y la desviación estándar *tiende* a la desviacíon estándar $\sigma$ de la distribución. Notar, entonces, que existen 
+valores calculados (promedio, desviación estándar) y valores teóricos ($\mu$ y $\sigma$). Confundirlos entre sí es un 
+error común.
 
 """
 
@@ -240,7 +249,8 @@ muestras = np.random.normal(loc=mu, scale=sigma, size=100)
 print('Valor medio teorico:', mu, '. Valor medio calculado:', muestras.mean())
 print('Desviacion estandar teorica:', sigma, '. Desviacion estandar calculada:', muestras.std())
 
-"""Comparemos el histograma de las muestras y la distribución teórica, que graficaremos haciendo uso de la librería `SciPy`:"""
+"""Comparemos el histograma de las muestras y la distribución teórica, que graficaremos haciendo uso de la librería 
+`SciPy`:"""
 
 from scipy.stats import norm
 
@@ -259,11 +269,15 @@ plt.show()
 
 """## 2. Correlación
 
-Tenemos dos variables aleatorias $X$ e $Y$, de las cuales tenemos $n$ muestras de cada una, $x_1,x_2,..., x_n$ e $y_1,y_2,..., y_n$. Sus valores medios son $\bar{x}$ e $\bar{y}$, respectivamente. Definimos la Covarianza como
+Tenemos dos variables aleatorias $X$ e $Y$, de las cuales tenemos $n$ muestras de cada una, $x_1,x_2,..., 
+x_n$ e $y_1,y_2,..., y_n$. Sus valores medios son $\bar{x}$ e $\bar{y}$, respectivamente. Definimos la Covarianza como
 
 $$Cov(X,Y) = \sum_{i=1}^{n} \frac{(x_i - \bar{x})(y_i - \bar{y})}{n}$$
 
-A veces verás que, en lugar de dividir por $n$, se divide por $n - 1$ ó $n - 2$, pero eso no es importante ahora. La covarianza es un valor que indica el grado de variación conjunta de dos variables aleatorias respecto a sus medias. Es el dato básico para determinar si existe una dependencia entre ambas variables y además es el dato necesario para estimar otros parámetros básicos, como el coeficiente de correlación lineal o la recta de regresión.
+A veces verás que, en lugar de dividir por $n$, se divide por $n - 1$ ó $n - 2$, pero eso no es importante ahora. La 
+covarianza es un valor que indica el grado de variación conjunta de dos variables aleatorias respecto a sus medias. 
+Es el dato básico para determinar si existe una dependencia entre ambas variables y además es el dato necesario para 
+estimar otros parámetros básicos, como el coeficiente de correlación lineal o la recta de regresión.
 
 Empezamos generandos muestras al azar de dos variables aleatorias no relacionadas entre sí.
 """
@@ -281,12 +295,16 @@ plt.xlim([-60, 60])
 plt.ylim([-60, 60])
 plt.show()
 
-"""¿Hay alguna relación entre ellos? Por relación nos referimos a "variación conjunta". Y por "variación conjunta" podemos imaginarnos que si una de las variables aumenta, la otra también lo hace. Y si una variable disminuye su valor, la otra también lo hace. La covarianza intenta cuantificar esa relación."""
+"""¿Hay alguna relación entre ellos? Por relación nos referimos a "variación conjunta". Y por "variación conjunta" 
+podemos imaginarnos que si una de las variables aumenta, la otra también lo hace. Y si una variable disminuye su 
+valor, la otra también lo hace. La covarianza intenta cuantificar esa relación."""
 
 cov = np.sum((x - x.mean()) * (y - y.mean())) / x.size
 print(cov)
 
-"""La covarianza, sin embargo, tiene un pequeño problema: depende de la escala de nuestros datos. Entonces, para deshacernos de la escala, se puede definir la Correlación, que no es otra cosa que la covarianza dividida la desviación estándar de cada variable aletaria.
+"""La covarianza, sin embargo, tiene un pequeño problema: depende de la escala de nuestros datos. Entonces, 
+para deshacernos de la escala, se puede definir la Correlación, que no es otra cosa que la covarianza dividida la 
+desviación estándar de cada variable aletaria.
 
 $$Corr(X,Y) = \frac{Cov(X,Y)}{\sigma_X \sigma_Y}$$
 """
@@ -294,14 +312,16 @@ $$Corr(X,Y) = \frac{Cov(X,Y)}{\sigma_X \sigma_Y}$$
 corr = cov / (x.std() * y.std())
 print(corr)
 
-"""Y con eso nos deshacemos de la escala. Un valor cercano a cero nos indica que no existe una relación (¿lineal?) entre las variables.
+"""Y con eso nos deshacemos de la escala. Un valor cercano a cero nos indica que no existe una relación (¿lineal?) 
+entre las variables.
 
-**Probar** con distintas escalas (modificando `sigma_1` y `sigma_2`) y verán que `cov` tomará valores en un rango muy amplio, mientras que `corr` se mantendrá cercana a cero.
+**Probar** con distintas escalas (modificando `sigma_1` y `sigma_2`) y verán que `cov` tomará valores en un rango muy 
+amplio, mientras que `corr` se mantendrá cercana a cero.
 
 ### 2.1 Relación lineal
 
-Veamos otro ejemplo: sabemos que existe una relación lineal entre $X$ e $Y$, es decir, podemos aproximar $Y =aX+b$, donde $a$ y $b$ son la pendiente y la ordenada al origen.
-"""
+Veamos otro ejemplo: sabemos que existe una relación lineal entre $X$ e $Y$, es decir, podemos aproximar $Y =aX+b$, 
+donde $a$ y $b$ son la pendiente y la ordenada al origen."""
 
 n = 100
 x = np.linspace(-1, 1, n) + 0.25 * np.random.normal(size=n)
@@ -322,14 +342,18 @@ print(corr)
 
 """#### Conclusiones
 
-1. La covarianza es una medida de la variación conjunta de dos variables. Pero tiene un problema: depende de la escala.
-2. Para "deshacernos" de la escala, definimos la correlación, que es simplemente la covarianza dividida por el producto de la desviación estándar de cada variable.
-3. La correlación es un valor entre -1 y 1. La correlación toma un valor cercano a uno cuando hay una relación lineal creciente entre las variables, cero cuando no hay relación y -1 cuando hay una relación lineal decreciente.
+1. La covarianza es una medida de la variación conjunta de dos variables. Pero tiene un problema: depende de la 
+escala. 2. Para "deshacernos" de la escala, definimos la correlación, que es simplemente la covarianza dividida por 
+el producto de la desviación estándar de cada variable. 3. La correlación es un valor entre -1 y 1. La correlación 
+toma un valor cercano a uno cuando hay una relación lineal creciente entre las variables, cero cuando no hay relación 
+y -1 cuando hay una relación lineal decreciente.
 
 ### 2.2 Covarianza y Correlación con NumPy
 
-NumPy ya tiene incorporadas funciones que calculan la covarianza y la correlación entre dos variables. La única diferencia es que, en lugar de devolver un único valor, devuelve cuatro valores, que corresponden a la covarianza/correlación entre $X$ con $X$, $X$ con $Y$, $Y$ con $X$, e $Y$ con $Y$. ¿Por qué, en la correlación, algunos valores son exactamente uno (1.)?
-"""
+NumPy ya tiene incorporadas funciones que calculan la covarianza y la correlación entre dos variables. La única 
+diferencia es que, en lugar de devolver un único valor, devuelve cuatro valores, que corresponden a la 
+covarianza/correlación entre $X$ con $X$, $X$ con $Y$, $Y$ con $X$, e $Y$ con $Y$. ¿Por qué, en la correlación, 
+algunos valores son exactamente uno (1.)?"""
 
 np.cov([x, y])
 
@@ -366,8 +390,6 @@ Por eso es muy importante graficar.
 
 Para eso, volvemos a usar el Iris Dataset.
 """
-
-import pandas as pd
 
 data = pd.read_csv('../../data/Iris.csv')
 data.drop(columns=['Id', 'Species'], inplace=True)
--- scripts/classes/introduction.py
+++ scripts/classes/introduction.py
@@ -78,14 +78,14 @@ else:
 
 a = 20
 b = 4
-print(a+b)
-print(a-b)
+print(a + b)
+print(a - b)
 
 a = 20
 b = 4.5
 
-c = a+b
-d = a-b
+c = a + b
+d = a - b
 
 print(c)
 print(d)
@@ -93,8 +93,8 @@ print(d)
 a = 20
 b = 4.5
 
-c = a+b
-d = a-b
+c = a + b
+d = a - b
 
 print(c)
 print(d)
@@ -104,20 +104,20 @@ print(type(d))
 
 x = 10
 y = 3
-z = x/y
+z = x / y
 print(z)
 
 print(type(z))
 
 x = 10
 y = 3
-print(x%y)
-print(type(x%y))
+print(x % y)
+print(type(x % y))
 
 # Ejercicio: Calcular el porcentaje que representa el valor 17 sobre un total de 54 y guardarlo en una variable llamada
 # porcentaje.
 # COMPLETAR
-porcentaje = 17/54*100
+porcentaje = 17 / 54 * 100
 print(porcentaje)
 
 # Ejercicio: calcula la suma y multiplicación de las variables a, b y c.
@@ -134,22 +134,22 @@ print(lista_1)
 
 len(lista_1)
 
-lista_2 = [5, 6.0,'Un poco de texto',-5, False, 'Más texto', True, 100]
+lista_2 = [5, 6.0, 'Un poco de texto', -5, False, 'Más texto', True, 100]
 
 print(lista_2[0])
 print(lista_2[1])
 print(lista_2[2])
 
-lista_2 = [5, 6.0,'Un poco de texto',-5, False, 'Más texto', True, 100]
+lista_2 = [5, 6.0, 'Un poco de texto', -5, False, 'Más texto', True, 100]
 # COMPLETAR
 print(lista_2[6])
 
-lista_2 = [5, 6.0,'Un poco de texto',-5, False, 'Más texto', True, 100]
+lista_2 = [5, 6.0, 'Un poco de texto', -5, False, 'Más texto', True, 100]
 print(lista_2[0:4])
 print(lista_2[4:])
 print(lista_2[0:8:3])
 
-lista_2 = [5, 6.0,'Un poco de texto',-5, False, 'Más texto', True, 100]
+lista_2 = [5, 6.0, 'Un poco de texto', -5, False, 'Más texto', True, 100]
 print(lista_2[-1])
 print(lista_2[-2])
 print(lista_2[-8])
@@ -165,7 +165,7 @@ print(lista_3[3])
 # de lista_1. Debería darte como resultado True.
 # COMPLETAR
 lista_3 = [0, lista_1, 'Mas texto', lista_2]
-#print(lista_3[1][2])
+# print(lista_3[1][2])
 print(lista_3)
 print(lista_3[3][4])
 
@@ -180,7 +180,7 @@ print(len(lista_vacia))
 # Ejercicio: Defina una lista vacia llamada lista_nueva y agréguele (append()) los elementos a, b y c.
 a = 8
 b = 'world'
-c = [12,24.5,66]
+c = [12, 24.5, 66]
 
 lista_nueva = []
 lista_nueva.append(a)
@@ -189,23 +189,22 @@ lista_nueva.append(c)
 # COMPLETAR
 print(lista_nueva)
 
-lista_1 = [1,2,3,4]
+lista_1 = [1, 2, 3, 4]
 lista_2 = lista_1  # lista_2 ahora es igual a lista_1
-lista_2[-1] = 100 # el ultimo elemento de la lista_2 ahora es 100
+lista_2[-1] = 100  # el ultimo elemento de la lista_2 ahora es 100
 print(lista_1, lista_2)
 
-lista_numeros = [3,55,1,876,12]
+lista_numeros = [3, 55, 1, 876, 12]
 for elemento in lista_numeros:
     # Definimos el codigo dentro del for mediante la indentación
     # (Todo lo que este corrido un 'tab' a la derecha)
     print(elemento)
-print(elemento)
 
-lista_numeros = [3,55,1,876,12]
-i = 0 # La iniciamos en cero
+lista_numeros = [3, 55, 1, 876, 12]
+i = 0  # La iniciamos en cero
 
 for elemento in lista_numeros:
-    i = i+1 # Le sumamos 1 cada vez que entra
+    i = i + 1  # Le sumamos 1 cada vez que entra
     print(elemento, i)
 
 lista_nombres = ['Ernesto', 'Camilo', 'Violeta']
@@ -219,40 +218,40 @@ for item in lista_nombres:
 print(nueva_lista)
 print(len(nueva_lista))
 
-numeros = [0,1,2,3,4,5,6,7,8,9]
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 # print(numeros + 3)
 
 # COMPLETAR 1
 for number in numeros:
-  print(number + 3)
+    print(number + 3)
 
 # COMPLETAR 2
 new_list = []
 for number in numeros:
-  new_list.append(number + 3)
+    new_list.append(number + 3)
 
 print(new_list)
 
 # Ejercicio 3: Genere una lista llamada numerosgrandes que contenga el cuadrado de cada elemento en la lista numeritos.
-numeritos = [3,1,5,7,12,10,17,4,22]
+numeritos = [3, 1, 5, 7, 12, 10, 17, 4, 22]
 # COMPLETAR
 numerosgrandes = []
 for number in numeritos:
-  numerosgrandes.append(number ** 2)
+    numerosgrandes.append(number ** 2)
 
 print(numerosgrandes)
 
 # Ejercicio 4: qué ocurre si sumas,+, dos listas. ¿Cómo se llama esa operación?
-lista_1 = [1,5,-8,3]
+lista_1 = [1, 5, -8, 3]
 lista_2 = [True, 'Cocodrilo que se duerme es cartera', 9, -17, 98, False]
 # COMPLETAR
 print(lista_1 + lista_2)
 
 # Ejercicio 5: sumar todos los elementos de la siguiente lista.
-numeros = [4,8,3,1,-3,3,-5,1,2,-8]
+numeros = [4, 8, 3, 1, -3, 3, -5, 1, 2, -8]
 # COMPLETAR
 suma = 0
 for number in numeros:
-  suma = suma + number
+    suma = suma + number
 
 print(suma)
